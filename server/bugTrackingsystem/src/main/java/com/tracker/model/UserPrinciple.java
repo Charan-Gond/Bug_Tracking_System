7104a2d996aada97d1781f9af29bbf850c2afa0b
Charan-Gond <charangond42@gmail.com>
2025-03-12 18:53:43 +0530

    user

--- server/bugTrackingsystem/src/main/java/com/tracker/model/UserPrinciple.java
+++ server/bugTrackingsystem/src/main/java/com/tracker/model/UserPrinciple.java
@@ -1,22 +1,37 @@
 package com.tracker.model;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserPrinciple implements UserDetails {
 
 
-    private User user;
-    public UserPrinciple(User u){
-        user =u;
+    private final User user;
+
+    private List<String> roles=new ArrayList<>();
+
+    public UserPrinciple(User u) {
+//        System.out.println("constructor");
+        user = u;
+
+       roles.addAll( List.of("DEVELOPER","ADMIN","TESTER","MANAGER"));
     }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority("developer"));
+
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
     @Override
